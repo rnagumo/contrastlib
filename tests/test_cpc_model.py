bd79cb1f6cb6227bcc79003a288304593099e23c
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-08-31 11:50:18 +0900

    Update test cases using pytest

--- tests/test_cpc_model.py
+++ tests/test_cpc_model.py
@@ -1,30 +1,21 @@
 
-import unittest
-
 import torch
-
 import contrastlib
 
 
-class TestContrastivePredictiveModel(unittest.TestCase):
+def test_cpc_forward() -> None:
+    model = contrastlib.ContrastivePredictiveModel()
+    x = torch.rand(8, 6, 3, 64, 64)
+    c = model(x)
 
-    def setUp(self):
-        self.model = contrastlib.ContrastivePredictiveModel()
-
-    def test_forward(self):
-        x = torch.rand(8, 6, 3, 64, 64)
-        c = self.model(x)
-
-        self.assertTupleEqual(c.size(), (8, 6, 10))
-
-    def test_loss_func(self):
-        x_p = torch.rand(8, 6, 3, 64, 64)
-        x_n = torch.rand(8, 6, 3, 64, 64)
-        loss_dict = self.model.loss_func(x_p, x_n)
-
-        self.assertIsInstance(loss_dict, dict)
-        self.assertFalse(torch.isnan(loss_dict["loss"]).any())
+    assert c.size() == (8, 6, 10)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_cpc_loss_func() -> None:
+    model = contrastlib.ContrastivePredictiveModel()
+    x_p = torch.rand(8, 6, 3, 64, 64)
+    x_n = torch.rand(8, 6, 3, 64, 64)
+    loss_dict = model.loss_func(x_p, x_n)
+
+    assert isinstance(loss_dict, dict)
+    assert not torch.isnan(loss_dict["loss"]).any()
